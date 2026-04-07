97b7276d3e007a9988f08d649b0f40a15d162a5e
acer20246 <2250512976@qq.com>
2026-04-07 23:14:27 +0800

    W1: add CMake skeleton and tcp broker/producer

---
+++ build/CMakeFiles/broker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/broker.dir/cmd/broker/main.cpp.o"
+  "CMakeFiles/broker.dir/cmd/broker/main.cpp.o.d"
+  "broker"
+  "broker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/broker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mq.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mq.dir/src/net.cpp.o"
+  "CMakeFiles/mq.dir/src/net.cpp.o.d"
+  "libmq.a"
+  "libmq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
